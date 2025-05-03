1dbb44a48aeea51da47859888c1b26e3edea9fa4
Xp04 <54464815+Xp04@users.noreply.github.com>
2025-05-03 12:35:30 -0500

    Added rough level design, terrain, trees, etc. Added heart and checkpoint prefabs

---
+++ Assets/Scripts/player_movement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class player_movement : MonoBehaviour
+{
+    public float speed = 6f;
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveX, 0, moveZ) * speed;
+        Vector3 newVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+        rb.linearVelocity = newVelocity;
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+}
+
